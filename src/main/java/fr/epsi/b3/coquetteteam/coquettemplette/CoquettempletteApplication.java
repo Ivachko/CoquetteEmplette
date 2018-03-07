1d87bbbaf37fb148b170dc724d89c258edb96f6a
Pierre <strobelpierre@gmail.com>
2018-03-07 15:19:16 +0100

    Change projet

--- src/main/java/fr/epsi/b3/coquetteteam/coquettemplette/CoquettempletteApplication.java
+++ src/main/java/fr/epsi/b3/coquetteteam/coquettemplette/CoquettempletteApplication.java
@@ -1,12 +1,12 @@
-package fr.epsi.coquetteteam.coquetteemplette;
+package fr.epsi.b3.coquetteteam.coquettemplette;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CoquetteempletteApplication {
+public class CoquettempletteApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CoquetteempletteApplication.class, args);
+        SpringApplication.run(CoquettempletteApplication.class, args);
     }
 }
--- src/test/java/fr/epsi/b3/coquetteteam/coquettemplette/CoquettempletteApplicationTests.java
+++ src/test/java/fr/epsi/b3/coquetteteam/coquettemplette/CoquettempletteApplicationTests.java
@@ -1,4 +1,4 @@
-package fr.epsi.coquetteteam.coquetteemplette;
+package fr.epsi.b3.coquetteteam.coquettemplette;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CoquetteempletteApplicationTests {
+public class CoquettempletteApplicationTests {
 
     @Test
     public void contextLoads() {
