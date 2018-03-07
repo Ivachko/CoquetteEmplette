52034c5712c148ef4c1ee2cdae92acc702e339d1
Pierre <strobelpierre@gmail.com>
2018-03-07 14:27:16 +0100

    Git Init

---
+++ src/main/java/fr/epsi/coquetteteam/coquetteemplette/CoquetteempletteApplication.java
@@ -0,0 +1,12 @@
+package fr.epsi.coquetteteam.coquetteemplette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoquetteempletteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoquetteempletteApplication.class, args);
+    }
+}
